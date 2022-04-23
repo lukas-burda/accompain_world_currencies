7405847c10f0cc94b2024bded1f29dd196c4695e
lukas-burda <contatolukasferreira@gmail.com>
2022-04-22 23:19:14 -0300

    Criação das models

---
+++ CSharpAPI/accompain_world_currencies_Core/Domain/Models/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accompain_world_currencies_Core.Domain.Models
+{
+    internal class Currency
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ CSharpAPI/accompain_world_currencies_Core/Domain/Models/Wallet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accompain_world_currencies_Core.Domain.Models
+{
+    internal class Wallet
+    {
+        public double Value { get; set; }
+        public string Name { get; set; }
+        public string CurrencyCodeBased { get; set; }
+        public List<Currency> accompaniedCurrencies { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
