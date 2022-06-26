40dd6e4edacb23d3c50762240a7ebe39a652e6ea
lukas-burda <contatolukasferreira@gmail.com>
2022-06-26 15:14:52 -0300

    Python consuming and producing with success

--- PythonProducerAndConsumer/QueueConsumer&Producer.py
+++ PythonProducerAndConsumer/QueueConsumer&Producer.py
@@ -5,7 +5,7 @@ import pika
 # Azure Queue Consumer
 #
 def AzureQueueConsumer(connStr, queueName):
-    servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
+    servicebus_client = ServiceBusClient.from_connection_string(conn_str=connStr, logging_enable=True)
     with ServiceBusClient.from_connection_string(connStr) as client:
         with client.get_queue_receiver(queueName) as receiver:
             received_message_array = receiver.receive_messages(max_message_count=5, max_wait_time=10)  # try to receive a single message within 10 seconds
@@ -20,22 +20,28 @@ def AzureQueueConsumer(connStr, queueName):
 #           RabbitMQ Queue Sender
 #  
 def RabbitQueueMessageProducer(msg):
+    stringmsg = str(msg)
+    
     if(msg is not None):
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         channel = connection.channel()
         channel.queue_declare(queue='mensagem')
         channel.basic_publish(exchange='',
                             routing_key='mensagem',
-                            body=msg)
+                            body= stringmsg.encode('utf-8') 
+                            )
         print(" [x] Sent"+str(msg))
     else:
         print("Empty string from queue")
 
-CONNECTION_STR = "Endpoint=sb://dconto.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=aDAmnosCtn3w9p91CBhuKy6ezLctiwsYwRCNhfq67yo="
-QUEUE_NAME = "availablecurrencies-queue"
+def main(CONNECTION_STR, QUEUE_NAME):
+    while(True):
+        print("Start queue processes...")
+        #AzureQueueConsumer(CONNECTION_STR, QUEUE_NAME)
+        RabbitQueueMessageProducer(AzureQueueConsumer(CONNECTION_STR, QUEUE_NAME))
+        print("Finished")
 
-while(True):
-    print("Start queue processes...")
-    #AzureQueueConsumer(CONNECTION_STR, QUEUE_NAME)
-    RabbitQueueMessageProducer(AzureQueueConsumer(CONNECTION_STR, QUEUE_NAME))
-    print("Finished")
+main(
+    "Endpoint=sb://dconto.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=aDAmnosCtn3w9p91CBhuKy6ezLctiwsYwRCNhfq67yo=",
+    "availablecurrencies-queue"
+    )
